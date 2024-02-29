7ad01ae2f29b8ccd59bd2fb64346e6b240692419
央旺 佐藤 <220511@jc-21.jp>
2024-02-29 11:38:09 +0900

    Main内の記述

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,37 @@
-int main() {
+#include <iostream>
+#include <string>
+#include "Functions.h"
 
+int main() {
+	while (true) {
+		std::cout << "input your side!(server : s, client : c) : ";
+		std::string input;
+		while (true) {
+			std::cin >> input;
+			if (input == "s" || input == "c")
+				break;
+			std::cout << "input your side!(server : s, client : c) : ";
+		}
+
+		if (input == "s") {
+			Server();
+		}
+		else {
+			Client();
+		}
+
+		std::cout << "if you want to play once more, input Y, or want to shutdown to input N : ";
+		while (true) {
+			std::cin >> input;
+			if (input == "Y" || input == "N")
+				break;
+			std::cout << "if you want to play once more, input Y, or want to shutdown to input N : ";
+		}
+		if (input == "N")
+			break;
+	}
+
+	std::cout << "Thank You For Playing My Game!" << std::endl;
 
 	return 0;
 }

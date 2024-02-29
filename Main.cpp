025baa5485b977ebe1a828360da3f450b05dd33e
央旺 佐藤 <220511@jc-21.jp>
2024-02-29 09:31:59 +0900

    サーバ・クライアントの選択

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <string>
+
 int main() {
+	std::cout << "input your side!(server : s, client : c) : ";
+	std::string input;
+	while (true) {
+		std::cin >> input;
+		if (input == "s" || input == "c")
+			break;
+		std::cout << "input your side!(server : s, client : c) : ";
+	}
 
 
 	return 0;
